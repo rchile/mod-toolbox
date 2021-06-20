9910707cc8b2d2f8f95fb63816dd0e4e6e19c0cc
Jonathan Gutierrez <contacto@jgutierrez.cl>
2021-06-20 01:15:59 -0400

    Move collected_static to assets

--- system/settings.py
+++ system/settings.py
@@ -108,8 +108,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Message settings
 MESSAGE_TAGS = {
