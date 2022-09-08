d8eb2fa162830a135d043d9249878b30800bde55
Mak <contacto@jgutierrez.cl>
2022-09-08 15:11:08 -0400

    dotenv

--- system/settings.py
+++ system/settings.py
@@ -1,10 +1,15 @@
 import os
+
+from dotenv import load_dotenv
+import dj_database_url
 from django.contrib.messages import constants as messages
 
+load_dotenv()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = ''
-DEBUG = False
-ALLOWED_HOSTS = []
+SECRET_KEY = os.getenv('SECRET_KEY', '')
+DEBUG = os.getenv('DEBUG', '') == '1'
+ALLOWED_HOSTS = [h for h in os.getenv('ALLOWED_HOSTS', '').split(',') if h]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -44,15 +49,20 @@ TEMPLATES = [
         },
     },
 ]
+
 # Logging
 # https://docs.djangoproject.com/en/3.0/topics/logging/
+default_log_format = '%(asctime)s | %(name)-14s | %(levelname)-8s | %(message)s'
+default_log_notime = default_log_format[14:]
+log_time = os.getenv('LOG_FORMAT_TIME', '1') == '1'
+log_format = os.getenv('LOG_FORMAT', default_log_format if log_time else default_log_notime)
 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
         'console': {
-            'format': '%(asctime)s | %(name)-14s | %(levelname)-8s | %(message)s',
+            'format': log_format,
         }
     },
     'handlers': {
@@ -79,30 +89,21 @@ LOGGING = {
 
 WSGI_APPLICATION = 'system.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+database_url = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
+DATABASES = {'default': dj_database_url.config(default=database_url)}
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': f'django.contrib.auth.password_validation.{x}'} 
+    for x in [
+        'UserAttributeSimilarityValidator',
+        'MinimumLengthValidator',
+        'CommonPasswordValidator',
+        'NumericPasswordValidator'
+    ]
 ]
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Chile/Continental'
+TIME_ZONE = 'America/Santiago'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
@@ -121,19 +122,20 @@ MESSAGE_TAGS = {
 }
 
 # CORS headers
-CORS_URLS_REGEX = r'^/api/.*$'
-CORS_ORIGIN_WHITELIST = ['https://rchile.xyz']
+cors_urls_regex_env = os.getenv('CORS_URLS_REGEX')
+CORS_URLS_REGEX = r'^/api/.*$' if not cors_urls_regex_env else re.compile(cors_urls_regex_env)
+CORS_ORIGIN_WHITELIST = [h for h in os.getenv('CORS_ORIGIN_WHITELIST', 'https://rchile.xyz').split(',') if h]
 
 # Reddit settings
-REDDIT_APP_ID = ''
-REDDIT_APP_SECRET = ''
-REDDIT_APP_REFRESH = ''
-REDDIT_APP_REDIRECT = 'http://127.0.0.1:8000/session/return'
-REDDIT_APP_UA = 'rchile-mod-toolbox/0.0.0'
-REDDIT_DEFAULT_SUB = 'chile'
+REDDIT_APP_ID = os.getenv('REDDIT_APP_ID', '')
+REDDIT_APP_SECRET = os.getenv('REDDIT_APP_SECRET', '')
+REDDIT_APP_REFRESH = os.getenv('REDDIT_APP_REFRESH', '')
+REDDIT_APP_REDIRECT = os.getenv('REDDIT_APP_REDIRECT', 'http://127.0.0.1:8000/session/return')
+REDDIT_APP_UA = os.getenv('REDDIT_APP_UA', 'rchile-mod-toolbox/0.0.0')
+REDDIT_DEFAULT_SUB = os.getenv('REDDIT_DEFAULT_SUB', 'chile')
 
 # MongoDB URI
-MODLOG_MONGODB_URI = 'mongodb://127.0.0.1:27015/entries'
+MODLOG_MONGODB_URI = os.getenv('MODLOG_MONGODB_URI', 'mongodb://127.0.0.1:27015/entries')
 
 try:
     from local_settings import *
